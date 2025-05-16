b7c932a3b5f007f58be39a9567a1abf958b830f3
Zahar Popov <zaharpopov949@gmail.com>
2025-05-16 20:50:53 +0300

    1 review

--- scripts/index.js
+++ scripts/index.js
@@ -2,17 +2,23 @@
 const cards = document.querySelector('.places__list');
 const cardTemplate = document.querySelector('#card-template').content;
 
-function addCard(title, img){
-   const card = cardTemplate.querySelector('.card').cloneNode(true);
-   card.querySelector('.card__title').textContent = title;
-   card.querySelector('.card__image').src = img;
-   cards.append(card);
-   const delbutton = card.querySelector('.card__delete-button');
+function createCard(title, img, func){
+    const card = cardTemplate.querySelector('.card').cloneNode(true);
+    card.querySelector('.card__title').textContent = title;
+    card.querySelector('.card__image').src = img;
+    card.querySelector('.card__image').alt = `Фотография места: ${title}`;
+    const delbutton = card.querySelector('.card__delete-button');
+   
 
-delbutton.addEventListener('click', function(evt) {
-    const eventTarget = evt.target;
-    cardDelete(eventTarget);
-})
+    delbutton.addEventListener('click', function(evt) {
+        const eventTarget = evt.target;
+        cardDelete(eventTarget);
+    })
+    return card
+}
+
+function addCard(card) {
+    cards.append(card); 
 }
 
 function cardDelete (delbutton) {
@@ -21,9 +27,13 @@ function cardDelete (delbutton) {
 }
 
 for (let i = 0; i < initialCards.length; i++) {
-    addCard(initialCards[i].name, initialCards[i].link);
+    createCard(initialCards[i].name, initialCards[i].link);
 }
 
+initialCards.forEach((element) => {
+    const card = createCard(element.name, element.link, cardDelete);
+    addCard(card)
+})
 
 // @todo: DOM узлы
 
